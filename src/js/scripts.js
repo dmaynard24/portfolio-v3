9fa0f93cce11fcf1b2ae5de6ce80705d7345ae72
dmaynard24 <davemaynard24@gmail.com>
2017-09-03 21:37:05 -0400

    intercepting scroll events. adding background animations

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,90 @@
 $(function() {
+    var pageNumber = 1;
+    var pageCount = $('.page').length;
+
     function init() {
         $('.fade-in-down').each(function() {
-            console.log($(this));
             $(this).addClass('active');
         });
     }
 
     init();
-    
-    function snapToPage() {
-        $('.page').each(function(i, e) {
-            console.log(e);
-        });
+
+    var lastMousewheelTime = 0;
+    $(window).bind('mousewheel', function(e) {
+        if (lastMousewheelTime === 0) {
+            lastMousewheelTime = Math.round(e.timeStamp);
+            if (e.originalEvent.wheelDelta > 0) {
+                snapToPage('up');
+            } else {
+                snapToPage('down');
+            }
+        } else {
+            var thisTime = Math.round(e.timeStamp);
+            if (thisTime > lastMousewheelTime + 1500) {
+                lastMousewheelTime = thisTime;
+                if (e.originalEvent.wheelDelta > 0) {
+                    snapToPage('up');
+                } else {
+                    snapToPage('down');
+                }
+            }
+        }
+    });
+
+    var touchDown;
+    var touchDownTime;
+    $(window).bind('touchstart', function(e) {
+        touchDown = e.originalEvent.touches[0].clientY;
+        touchDownTime = Math.round(e.timeStamp);
+    });
+
+    $(window).bind('touchend', function(e) {
+        var touchUp = e.originalEvent.changedTouches[0].clientY;
+        var touchUpTime = Math.round(e.timeStamp);
+        if (touchUpTime - touchDownTime > 1000) { // hesitated touchmove
+            if (touchUp > touchDown + 30) {
+                snapToPage('up');
+            } else if (touchUp < touchDown - 30) {
+                snapToPage('down');
+            }
+        } else { // instant touchmove
+            if (touchUp > touchDown + 15) {
+                snapToPage('up');
+            } else if (touchUp < touchDown - 15) {
+                snapToPage('down');
+            }
+        }
+    });
+
+    function snapToPage(direction) {
+        var cancel = false;
+        var lastPageNumber = pageNumber;
+
+        if (direction == 'up') {
+            if (pageNumber !== 1) {
+                pageNumber--;
+            } else {
+                cancel = true;
+            }
+        } else if (direction == 'down') {
+            if (pageNumber !== pageCount) {
+                pageNumber++;
+            } else {
+                cancel = true;
+            }
+        }
+        
+        if (!cancel) {
+            $('.page-background-container').addClass('scale-down');
+            var lastPageBackground = $('.page[data-page-number="' + lastPageNumber + '"] .page-background');
+            setTimeout(function() {
+                lastPageBackground.addClass('hide-up');
+            }, 750);
+            var scrollTo = $('.page[data-page-number="' + pageNumber + '"]')[0].offsetTop;
+            $('.fullscreen').animate({
+                scrollTop: scrollTo
+            }, 1500);
+        }
     }
 });
